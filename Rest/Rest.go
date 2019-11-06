14044c54e75e37faf56f1e6715218153a357c76c
Oleg Kutyumov <Novarn3@yandex.ru>
2019-11-06 15:05:03 +0300

    learning

--- Rest/Rest.go
+++ Rest/Rest.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"database/sql"
+	"Rest/Connector"
 	"encoding/json"
-	_ "encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	_ "github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"strconv"
 )
 
-
-	
-	type qwerty struct {
-		id   int
-		Name string
-	}
-
-	
 	func main() {
 		router := mux.NewRouter()
 		router.HandleFunc("/Name/{Name}/{id:[0-9]+}", trouble)
@@ -38,38 +27,12 @@ func trouble(w http.ResponseWriter, r *http.Request) {
 		log.Println("Get info about id #", id)
 	}
 
-		db, name := connect(id)
+		db, name := Connector.Connect(id)
 		defer db.Close()
+
 		json.NewEncoder(w).Encode(name)
 
 }
-	func connect(id	int) (*sql.DB, []qwerty) {
-		connStr := "user=sa password=sa dbname=testdb sslmode=disable"
-		db, err := sql.Open("postgres", connStr)
-		if err != nil {
-			panic(err)
-		}
 
-		rows, err := db.Query("select * from t_itfb WHERE id = $1", id)
-		if err != nil {
-			panic(err)
-		}
-		defer rows.Close()
-		var Name []qwerty
 
-		for rows.Next() {
-			p := qwerty{}
-			err := rows.Scan(&p.id, &p.Name)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			Name = append(Name, p)
-		}
-		//Запихнул все в переменную Name
-		for _, p := range Name {
-			fmt.Println(p.id, p.Name)
 
-		}
-		return db, Name
-	}
